aee24bffdc983aa917f465f83a3dd255d8ba3ddd
Kayque 2 -  Win 11 <kayquelopes@outlook.com.br>
2022-12-19 12:44:54 -0300

    Player: add Movement, Jump, DoubleJumpand Animations!

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,13 @@ public class Player : MonoBehaviour
 {
 
     private Rigidbody2D rig;
+    public Animator anim;
+
     public float speed;
+    public float jumpForce;
+
+    private bool isJumping;
+    private bool doubleJumping;
 
 
     // Start is called before the first frame update
@@ -18,7 +24,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Jump();
     }
 
     void FixedUpdate()
@@ -36,12 +42,53 @@ public class Player : MonoBehaviour
 
         if (movement > 0)
         {
-            transform.eulerAngles = new Vector3(0, 0, 0);
+             if (!isJumping)
+             {
+                anim.SetInteger("transaction", 1);
+             }
+                transform.eulerAngles = new Vector3(0, 0, 0);
         }
 
         if (movement < 0)
         {
-            transform.eulerAngles = new Vector3(0, 180, 0);
+            if (!isJumping)
+            {
+                anim.SetInteger("transaction", 1);
+            }
+                transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+
+        if (movement == 0 && !isJumping)
+        {
+            anim.SetInteger("transaction", 0);
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (!isJumping)
+            {
+                anim.SetInteger("transaction", 2);
+                rig.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                isJumping = true;
+                doubleJumping = true;
+            } else if (doubleJumping)
+            {
+                anim.SetInteger("transaction", 2);
+                rig.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                doubleJumping = false;
+            }
+          
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == 8)
+        {
+            isJumping = false;
         }
     }
 }
