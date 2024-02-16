ec11d2ffbc9d38f3f8b674178bab1b492bfa322c
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2024-02-16 03:18:13 -0300

    Iniciando menus adicionais!

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject _mainMenu;
+    [SerializeField] private GameObject _optionsMenu;
+
+    private Boolean _inMainMenu = true;
+
     public void StartGame()
     {
         SceneManager.LoadScene(1);
@@ -13,5 +19,21 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void toggleMenu(GameObject targetMenu)
+    {
+        if (_inMainMenu)
+        {
+            _mainMenu.SetActive(false);
+            targetMenu.SetActive(true);
+            _inMainMenu = false;
+        }
+        else
+        {
+            _mainMenu.SetActive(true);
+            targetMenu.SetActive(false);
+            _inMainMenu = true;
+        }
     }
 }
